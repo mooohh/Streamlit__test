0c368e30456328dd993e5305a5c357605de15ff9
Mohamed <74993038+mooohh@users.noreply.github.com>
2023-04-28 21:57:35 +0200

    Update deploy_model.py

--- deploy_model.py
+++ deploy_model.py
@@ -2,16 +2,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import datetime
+import json
+import folium 
+from streamlit_folium import st_folium
 
 def user_inputs():
     date = st.date_input(
-        "Date Of The Predictions",
+        "Select Date",
         datetime.date(2023, 4, 29))
     
     speed = st.sidebar.slider('The Speed', 0, 200, 60)
 
     data = pd.DataFrame({'date': date, 'speed': speed}, index=[0])
-
 
     return data
 
@@ -21,10 +23,35 @@ st.write('''
          
          ''')
 
-st.sidebar.header('Les parametres d entrée' )
+st.sidebar.header('The inputs' )
 
 data = user_inputs()
 st.subheader('We will predict the status of all the streets in nantes city')
 st.write(data)
+
+
+
+with open('coordinates.json', 'r') as f:
+    coordinates = json.load(f)
+
+c = folium.Map(location = [47.219251, -1.554291],)
+
+
+i = 0
+for coordinate in coordinates:
+    # Dessiner une ligne rouge le long de la Rue de Strasbourg
+    if i <= 600:
+        folium.PolyLine(coordinate, color="green").add_to(c)
+    elif i < 650:
+        folium.PolyLine(coordinate, color="blue").add_to(c)
+    elif i < 700:
+        folium.PolyLine(coordinate, color="red").add_to(c)
+    else:
+        folium.PolyLine(coordinate, color="orange").add_to(c)
+    i += 1 
+        
+
+# call to render Folium map in Streamlit
+st_data = st_folium(c, width=725)
 
 
