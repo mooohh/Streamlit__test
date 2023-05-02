38b3425e3c6df5b95c31116c87f8bf6ceba0aaba
Mohamed <74993038+mooohh@users.noreply.github.com>
2023-05-02 17:02:25 +0200

    Update deploy_model.py

--- deploy_model.py
+++ deploy_model.py
@@ -3,57 +3,66 @@ import pandas as pd
 import numpy as np
 import datetime
 import json
-
-
 import folium 
+import pickle
 from streamlit_folium import st_folium
+from PIL import Image
+from functions import predict_status, download_models, user_inputs, inverse_coordinate
 
-def user_inputs():
-    date = st.date_input(
-        "Select Date",
-        datetime.date(2023, 4, 29))
-    
-    speed = st.sidebar.slider('The Speed', 0, 200, 60)
+# Render the image with a link
 
-    data = pd.DataFrame({'date': date, 'speed': speed}, index=[0])
+link_url = "https://www.mis.u-picardie.fr/"
 
-    return data
+image_url = "https://home.mis.u-picardie.fr/~devismes/WWW/images/mis.png"
 
+st.markdown(f'<a  href="{link_url}" target="_blank"><img  src="{image_url}" width="130"></a>', unsafe_allow_html=True)
 
 st.write('''
-         # App Simple pour la prédiction des états des rue de la ville de nantes 
-         
+         ## App For Predicting The Status Of The :red[Nantes] Streets. 
          ''')
 
-st.sidebar.header('The inputs' )
+st.sidebar.header('The Inputs' )
 
 data = user_inputs()
-st.subheader('We will predict the status of all the streets in nantes city')
-st.write(data)
+st.write('The Parametres That You Already Choose')
+st.write(data) 
 
-
-
-with open('coordinates.json', 'r') as f:
-    coordinates = json.load(f)
 
 c = folium.Map(location = [47.219251, -1.554291],)
 
 
-i = 0
-for coordinate in coordinates:
+final_result = download_models(data)
+
+colors = final_result['couleur_tp']
+coordinates = final_result['geo_shape']
+
+color_dict = { 'Fluide    ' : 'green', 'Indéterminé      ' : 'blue', 'Dense      ': 'black', 'Saturé      ' : 'yellow', 'Bloqué      ' : 'red'}
+
+text = ''
+for name, color in color_dict.items():
+    text += f'<div style="background-color: {color}; display: inline-block; width: 20px; height: 20px;"></div> {name}'
+    
+st.write(text, unsafe_allow_html=True)
+
+
+c = folium.Map(location = [47.219251, -1.554291],)
+
+for i, coordinate in enumerate(coordinates):
     # Dessiner une ligne rouge le long de la Rue de Strasbourg
-    if i <= 600:
+    coordinate = coordinate['coordinates']
+    color_number = colors[i] 
+    if  color_number == 3:
         folium.PolyLine(coordinate, color="green").add_to(c)
-    elif i < 650:
+    elif color_number == 2:
         folium.PolyLine(coordinate, color="blue").add_to(c)
-    elif i < 700:
-        folium.PolyLine(coordinate, color="red").add_to(c)
+    elif color_number == 4:
+        folium.PolyLine(coordinate, color="black").add_to(c)
+    elif color_number == 5:
+        folium.PolyLine(coordinate, color="yellw").add_to(c)
     else:
-        folium.PolyLine(coordinate, color="orange").add_to(c)
-    i += 1 
-        
+        folium.PolyLine(coordinate, color="red").add_to(c)
+
+
 
 # call to render Folium map in Streamlit
 st_data = st_folium(c, width=725)
-
-
